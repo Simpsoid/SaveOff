4eb943d5bfe0aaa7ce2b3b4014a5c64591844b61
Simpsoid <david@david.com>
2020-03-29 13:48:43 +1000

    Finalised details

--- herosaver-autoloader.user.js
+++ herosaver-autoloader.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name            Herosaver Autoloader
-// @author          TeaWithLucas
-// @namespace       https://github.com/TeaWithLucas/Herosaver/
-// @description     Autoloader for the Herosaver Script
+// @name            Save Off - DS
+// @author          Simpsoid
+// @namespace       https://github.com/Simpsoid/SaveOff
+// @description     Autoloader Save Off Script
 // @version	        1
 // @include         *example.com/*
 // @include         *forge.com/*
-// @installURL      https://raw.githubusercontent.com/TeaWithLucas/Herosaver/master/herosaver-autoloader.user.js
-// @downloadURL     https://raw.githubusercontent.com/TeaWithLucas/Herosaver/master/herosaver-autoloader.user.js
-// @updateURL       https://raw.githubusercontent.com/TeaWithLucas/Herosaver/master/herosaver-autoloader.user.js
+// @installURL      https://raw.githubusercontent.com/Simpsoid/SaveOff/master/herosaver-autoloader.user.js
+// @downloadURL     https://raw.githubusercontent.com/Simpsoid/SaveOff/master/herosaver-autoloader.user.js
+// @updateURL       https://raw.githubusercontent.com/Simpsoid/SaveOff/master/master/herosaver-autoloader.user.js
 // @run-at          document-end
 // ==/UserScript==
  
@@ -30,7 +30,7 @@
 //object constructor
 function runinsert(){
 
-  // run the Herosaver Script
+  // run the SaveOff Script
   var xhr=new XMLHttpRequest;xhr.open("get","https://raw.githubusercontent.com/Simpsoid/SaveOff/master/dist/saver.min.js",true);xhr.onreadystatechange=function(){if(xhr.readyState==4){var script=document.createElement("script");script.type="text/javascript";script.text=xhr.responseText;document.body.appendChild(script)}};xhr.send(null);
 
 };
